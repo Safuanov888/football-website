bc1c5112008db7865f50dd6f677ab754a96eb73c
msmir <maksim-smirnov-2004@inbox.ru>
2024-10-11 22:43:04 +0300

    add docker files

--- app/app/settings.py
+++ app/app/settings.py
@@ -85,11 +85,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
