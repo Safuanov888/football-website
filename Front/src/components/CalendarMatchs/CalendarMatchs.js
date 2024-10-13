893bfa34e9987c9db1816eecb87f5c7cbb48c201
Svetlana <74565522+SVETA12345@users.noreply.github.com>
2024-10-13 18:05:08 +0300

    Update CalendarMatchs.js

--- Front/src/components/CalendarMatchs/CalendarMatchs.js
+++ Front/src/components/CalendarMatchs/CalendarMatchs.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import './CalendarMatchs.css';
-
 import styled, { css } from 'styled-components';
 const Frame = styled.div`
   width: 400px;
@@ -50,29 +49,23 @@ const Day = styled.div`
       background-color: #eee;
     `}
     ${props =>
-      props.isEvent &&
-      css`
+    props.isEvent &&
+    css`
         background: red;
       `}
 `;
-export function CalendarMatchs({date, setDate}) {
+export function CalendarMatchs({ date, setDate, cardsMonth, setCurrentMonth, setCurrentDate }) {
   const today = new Date();
   const DAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   const DAYS_LEAP = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   const DAYS_OF_THE_WEEK = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
   const MONTHS = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-  const cardsData = [{ id: 0, date: "2024-10-05T15:00:00+03:00", command: { name: 'Тройка', score: '3:0', text_syperliga: "Суперлига FS сезон", adres: "Вернандка парк 1" } },
-    { id: 1, date: "2024-09-24", command: { name: 'Тройка', score: '3:0', text_syperliga: "Суперлига FS сезон", adres: "Вернандка парк 1" } },
-    { id: 2, date: "2024-09-24", command: { name: 'Тройка', score: '3:0', text_syperliga: "Суперлига FS сезон", adres: "Вернандка парк 1" } },
-    { id: 3, date: "2024-09-24", command: { name: 'Тройка', score: '3:0', text_syperliga: "Суперлига FS сезон", adres: "Вернандка парк 1" } },
-    { id: 4, date: "2024-09-24", command: { name: 'Тройка', score: '3:0', text_syperliga: "Суперлига FS сезон", adres: "Вернандка парк 1" } },
-    ]
-  
+
   const [day, setDay] = useState(date.getDate());
   const [month, setMonth] = useState(date.getMonth());
   const [year, setYear] = useState(date.getFullYear());
   const [startDay, setStartDay] = useState(getStartDayOfMonth(date));
- 
+
   useEffect(() => {
     setDay(date.getDate());
     setMonth(date.getMonth());
@@ -87,27 +80,33 @@ export function CalendarMatchs({date, setDate}) {
   function isLeapYear(year) {
     return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
   }
-  function handleIsEvent(year, month, d){
-    let isEvent=false
-    cardsData.forEach((match)=>{
-      const dateMatch= new Date(match.date)
-      if(dateMatch.getDate()===d && dateMatch.getMonth()===month && dateMatch.getFullYear()===year){
-        isEvent=true
+  function handleIsEvent(year, month, d) {
+    let isEvent = false
+    cardsMonth.forEach((match) => {
+      const dateMatch = new Date(match.date)
+      if (dateMatch.getDate() === d && dateMatch.getMonth() === month && dateMatch.getFullYear() === year) {
+        isEvent = true
       }
     })
     return isEvent
 
   }
   const days = isLeapYear(date.getFullYear()) ? DAYS_LEAP : DAYS;
-  
+
   return (
     <Frame>
       <Header>
-        <Button onClick={() => setDate(new Date(year, month - 1, day))}>Пред</Button>
+        <Button onClick={() => {
+          setCurrentMonth(new Date(year, month - 1, day));
+          setDate(new Date(year, month - 1, day))
+        }}>Пред</Button>
         <div>
           {MONTHS[month]} {year}
         </div>
-        <Button onClick={() => setDate(new Date(year, month + 1, day))}>След</Button>
+        <Button onClick={() => {
+          setCurrentMonth(new Date(year, month + 1, day));
+          setDate(new Date(year, month + 1, day))
+        }}>След</Button>
       </Header>
       <Body>
         {DAYS_OF_THE_WEEK.map(d => (
@@ -125,7 +124,11 @@ export function CalendarMatchs({date, setDate}) {
                 isToday={d === today.getDate() && month === today.getMonth() && year === today.getFullYear()}
                 isEvent={handleIsEvent(year, month, d)}
                 isSelected={false}
-                onClick={() => setDate(new Date(year, month, d))}
+                onClick={() => {
+                  setCurrentDate(new Date(year, month, d));
+                  setDate(new Date(year, month, d))
+                }
+                }
               >
                 {d > 0 ? d : ''}
               </Day>
