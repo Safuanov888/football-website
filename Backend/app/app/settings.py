c763b5358c272baea88dfeb2af86840f0ec7510f
Saifullin Bulat <134305871+bulatbulbul@users.noreply.github.com>
2024-10-14 23:36:42 +0300

    Update settings.py

--- Backend/app/app/settings.py
+++ Backend/app/app/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*(dv#s(jirhyc&al@@j2+h+)zs=i%6x$p@6z@wn)!@s8y$c8lf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
